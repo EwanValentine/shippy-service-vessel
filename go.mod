e690b04e5b636da449c7fdab05900dcc6c582c51
Ewan Valentine <ewan.valentine@peak.ai>
2019-04-16 08:52:00 +0100

    UPDATED to use new mgo

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/EwanValentine/shippy-service-vessel
+
+go 1.12
+
+require (
+	github.com/armon/circbuf v0.0.0-20190214190532-5111143e8da2 // indirect
+	github.com/golang/protobuf v1.3.1
+	github.com/mattn/go-colorable v0.1.1 // indirect
+	github.com/micro/go-micro v1.1.0
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3
+)
---
+++ handler.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	pb "github.com/EwanValentine/shippy-service-vessel/proto/vessel"
+)
+
+type handler struct {
+	repository
+}
+
+// FindAvailable vessels
+func (s *handler) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+
+	// Find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
+// Create a new vessel
+func (s *handler) Create(ctx context.Context, req *pb.Vessel, res *pb.Response) error {
+	if err := s.repository.Create(req); err != nil {
+		return err
+	}
+	res.Vessel = req
+	return nil
+}
